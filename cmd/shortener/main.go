a17ac2adbe815ee63a627bdf7d89a3ad0780866e
Алексей Фишлер <a.fishler@tapir.ws>
2025-11-06 21:33:18 +0300

    Iter3: Возврат дефолтного порта

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	st := storage.NewInMemoryStorage()
-	srv := webapi.NewServer(":6666", st)
+	srv := webapi.NewServer(":8080", st)
 
 	if err := srv.Run(context.Background()); err != nil {
 		log.Fatalf("server error: %v", err)
