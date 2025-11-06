596419cafa8a83afd302b3931f7a33e43835e98f
Алексей Фишлер <a.fishler@tapir.ws>
2025-11-07 00:05:16 +0300

    Iter4: Проверка пайпланов тестов

--- internal/config/main.go
+++ internal/config/main.go
@@ -21,7 +21,7 @@ var (
 func NewConfig() *Config {
 	once.Do(func() {
 		address := flag.String("a", "localhost:8080", "Адрес запуска HTTP-сервера (например, localhost:8080)")
-		baseURL := flag.String("b", "", "Базовый адрес для коротких ссылок (например, http://localhost:8080)")
+		baseURL := flag.String("b", "http://localhost:8080", "Базовый адрес для коротких ссылок (например, http://localhost:8080)")
 
 		flag.Parse()
 
