6d51807aee7ab967830a47ba79f774db1b221bf7
Алексей Фишлер <a.fishler@tapir.ws>
2025-12-12 21:53:02 +0300

    iter14: Static test

--- internal/ports/webapi/middleware/auth.go
+++ internal/ports/webapi/middleware/auth.go
@@ -9,19 +9,19 @@ import (
 	"github.com/google/uuid"
 )
 
-const CookieUserIdKey = "user_id"
+const CookieUserIDKey = "user_id"
 
 func AuthMiddleware(auth *auth.CookieAuth) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			c, err := r.Cookie(CookieUserIdKey)
+			c, err := r.Cookie(CookieUserIDKey)
 
 			if err != nil {
 				userID := uuid.New().String()
 				token := auth.Sign(userID)
 
 				http.SetCookie(w, &http.Cookie{
-					Name:     CookieUserIdKey,
+					Name:     CookieUserIDKey,
 					Value:    token,
 					Path:     "/",
 					HttpOnly: true,
