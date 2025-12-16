8f7022b2d54e17c2f361b4f675e557044216e23f
Алексей Фишлер <a.fishler@tapir.ws>
2025-12-16 21:25:40 +0300

    Revert "iter15: Упрощение healthcheck"
    This reverts commit bf1d0b861213566f17eb9d8b95de9d4f2d0b4d05.

--- internal/ports/webapi/handlers/healthcheck.go
+++ internal/ports/webapi/handlers/healthcheck.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/amberdance/url-shortener/internal/ports/webapi/middleware"
 	"github.com/go-chi/chi/v5"
 )
 
@@ -17,6 +18,8 @@ func (h *HealthcheckHandler) Routes() chi.Router {
 	router := chi.NewRouter()
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", middleware.ContentTypeJSONHeaderValue)
+		_, _ = w.Write([]byte(`{"status":"ok"}`))
 	})
 
 	return router
